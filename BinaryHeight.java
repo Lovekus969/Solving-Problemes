903308f6cd65198789f86c4cb5a94adc1dd69546
BHAVNEET BHAVNEET <Bhavneet11d@gmail.com>
2025-04-10 12:49:40 -0400

    BinaryTree-Height
    The program creates a binary tree using custom `Node` objects.
    - The height is calculated using recursive traversal of the left and right subtrees.
    - `Math.max()` is used to return the max depth + 1.

---
+++ BinaryHeight.java
@@ -0,0 +1,32 @@
+ class Node{
+    int data;
+    Node left;
+    Node right;
+    //call constructor
+    public Node(int data){
+        this.data=data;
+        this.left=null;
+        this.right=null;
+    }
+}
+public class BinaryHeight{
+    public static int treeHeight(Node root){
+        //base case 
+        if(root ==null){
+            return 0;
+        }
+        int leftpart =treeHeight(root.left);
+        int Rightpart = treeHeight(root.right);
+
+        int ans = Math.max(leftpart, Rightpart)+1;
+        return ans;
+    }
+    public static void main(String[] args){
+        Node root = new Node(3);
+        root.left = new Node(2);
+        root.right = new Node(1);
+        root.right.left =new Node(4);
+        root.right.right =new Node(5);
+       System.out.println("Height is "+ treeHeight(root));
+    }
+}
