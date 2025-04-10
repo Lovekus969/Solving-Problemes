02b9a731299c4333e65d3528bbb4952080c9a949
BHAVNEET BHAVNEET <Bhavneet11d@gmail.com>
2025-04-10 13:09:30 -0400

    Sum of all nodes
    Its logical and shows us how root.right and root.left works and how recursive approach is beneficial to get the and very nicely.

--- BinaryHeight.java
+++ BinaryHeight.java
@@ -21,17 +21,29 @@ public class BinaryHeight{
     //     int ans = Math.max(leftpart, Rightpart)+1;
     //     return ans;
     // }
-    public static int countNodes(Node root){//Works as post order Traversal 
-        //base case                         // left right root O(N)
+    // public static int countNodes(Node root){
+    //     //base case 
+    //     if(root==null){
+    //         return 0;
+    //     }
+    //     int Left_Count = countNodes(root.left);
+    //     int Right_Count = countNodes(root.right);
+
+    //     int ans = Left_Count+Right_Count+1;
+
+    //     return  ans;  
+    // }
+    public static int sum(Node root){
+        //base case 
         if(root==null){
             return 0;
         }
-        int Left_Count = countNodes(root.left);
-        int Right_Count = countNodes(root.right);
+        //call recursively for right and left portion to cal the sum 
+        int left_Sum = sum(root.left);
+        int right_Sum = sum(root.right);
 
-        int ans = Left_Count+Right_Count+1;
-
-        return  ans;  
+        int ans = left_Sum+right_Sum+root.data;
+        return ans;
     }
     public static void main(String[] args){
         Node root = new Node(3);
@@ -39,6 +51,6 @@ public class BinaryHeight{
         root.right = new Node(1);
         root.right.left =new Node(4);
         root.right.right =new Node(5);
-       System.out.println("Count is "+ countNodes(root));
+       System.out.println("Sum is "+ sum(root));
     }
 }
