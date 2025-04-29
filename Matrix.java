ac56ec978163bfe54da3c5ae016486cf30a64d3a
BHAVNEET BHAVNEET <Bhavneet11d@gmail.com>
2025-04-28 21:45:07 -0400

    Tabulation method
    Set dp[i][i] = 0
    2	Fill table diagonally from chain length 2 to n
    3	For each (i,j), find all possible k between i and j, and pick minimum cost
    4	Return dp[0][n-1]

--- Matrix.java
+++ Matrix.java
@@ -25,6 +25,30 @@ public class Matrix {
         dp[i][j] = ans; // store the result
         return dp[i][j];
     }
+public static int matrixChainMultiplication(int[] p) {
+    int n = p.length - 1; // number of matrices
+    int[][] dp = new int[n][n];
+
+    // initialize diagonal with 0
+    for (int i = 0; i < n; i++) {
+        dp[i][i] = 0;
+    }
+
+    // l = chain length
+    for (int len = 2; len <= n; len++) {
+        for (int i = 0; i <= n - len; i++) {
+            int j = i + len - 1;
+            dp[i][j] = Integer.MAX_VALUE;
+
+            for (int k = i; k < j; k++) {
+                int cost = dp[i][k] + dp[k+1][j] + p[i] * p[k+1] * p[j+1];
+                dp[i][j] = Math.min(dp[i][j], cost);
+            }
+        }
+    }
+
+    return dp[0][n-1]; // minimum cost to multiply matrices from A1 to An
+}
 
     public static void main(String[] args) {
         int arr[] = {1, 2, 3, 4, 3};
@@ -36,6 +60,6 @@ public class Matrix {
             Arrays.fill(row, -1);
         }
 
-        System.out.println("Minimum number of multiplications: " + mcm(arr, 1, n - 1, dp));
+        System.out.println("Minimum number of multiplications: " + matrixChainMultiplication(arr));
     }
 }
