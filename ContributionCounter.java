c50eaabfc1adfcb5a60ba5822cfdd45f4eb3f79c
BHAVNEET BHAVNEET <Bhavneet11d@gmail.com>
2025-08-12 02:07:48 -0400

    Add files via upload

--- ContributionCounter.java
+++ ContributionCounter.java
@@ -1,13 +1,13 @@
 public class ContributionCounter {
     public static void countTo700(int start) {
-        for (int i = start; i <= 700; i++) {
+        for (int i = start; i <= 707; i++) {
             System.out.print(i + " ");
         }
-        System.out.println("\nYes! Contribution 700 reached. Welcome 🎉");
+        System.out.println("\nYes! Contribution 700 reached. Welcome  Bhavneet ..");
     }
 
     public static void main(String[] args) {
         int currentContribution = 699;  // starting point
-        countTo700(currentContribution);
+     countTo700(currentContribution);
     }
 }
