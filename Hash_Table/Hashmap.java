203a691c60be57d61af47b6ca42c83b383608f69
unknown User 404 <Bhavneet11d@gmail.com>
2025-09-24 17:47:20 -0400

    Create Hashmap.java

---
+++ Hash_Table/Hashmap.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+     Hashtable<Integer , String> table = new Hashtable<>();
+     table.put(1,"Apple");
+     table.put(2,"Banana");
+     table.put(3,"papya");
+     
+     System.out.println(table.get(2));
+  }
+}
